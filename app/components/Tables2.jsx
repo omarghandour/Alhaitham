db229fd20c611ac6284a2243ce53d13583f5280d
omarghandour <alex.forget50@gmail.com>
2024-01-16 14:01:47 +0200

    menu edit

--- app/components/Tables2.jsx
+++ app/components/Tables2.jsx
@@ -11,12 +11,13 @@ import {
 import Image from "next/image";
 import im1 from "../../public/1.jpeg";
 import im2 from "../../public/2.jpeg";
-import im3 from "../../public/3.jpeg";
+import im3 from "../../public/مكرونة الهيثم1.png";
 import im4 from "../../public/4.jpeg";
 import im5 from "../../public/5.jpeg";
 import im6 from "../../public/6.jpeg";
 import im7 from "../../public/7.jpeg";
 import im8 from "../../public/8.jpeg";
+import im9 from "../../public/كريب الهيثم1.png";
 import Link from "next/link";
 import { Button } from "./ui/button";
 // import im9 from "../../public/9.jpeg";
@@ -25,32 +26,32 @@ const Tables2 = () => {
     {
       id: 1,
       name: "بطاطس",
-      prices: ["15", "20"],
+      prices: ["20", "25"],
     },
     {
       id: 2,
       name: "فلافل",
-      prices: ["15", ""],
+      prices: ["15", "20"],
     },
     {
       id: 3,
       name: "ميكس فلافل بطاطس",
-      prices: ["22", ""],
+      prices: ["25", ""],
     },
     {
       id: 4,
       name: "بطاطس بالبيض",
-      prices: ["21", ""],
+      prices: ["27", ""],
     },
     {
       id: 5,
       name: "بطاطس بالجبنة",
-      prices: ["25", ""],
+      prices: ["25", "30"],
     },
     {
       id: 6,
       name: "بطاطس لانشون",
-      prices: ["25", ""],
+      prices: ["25", "30"],
     },
     {
       id: 7,
@@ -81,82 +82,187 @@ const Tables2 = () => {
   const salads = [
     {
       id: 1,
-      name: "سلطة خضرا",
-      prices: ["20", ""],
+      name: "علبة تومية",
+      prices: ["10", "15-20"],
     },
     {
       id: 2,
-      name: "فتوش",
-      prices: ["20", ""],
-    },
-    {
-      id: 3,
-      name: "علبة تومية",
-      prices: ["10-15", "20"],
-    },
-    {
-      id: 4,
       name: "مسبحة",
       prices: ["10", "15"],
     },
     {
-      id: 5,
-      name: "طول سلو",
-      prices: ["10", "15"],
-    },
-  ];
-  const plates = [
-    {
-      id: 1,
-      name: "طبق بيض مقلي",
-      prices: ["20", "25"],
-    },
-    {
-      id: 2,
-      name: "طبق أرز بسمتي",
-      prices: ["25", "30"],
-    },
-    {
       id: 3,
+      name: "طبق ارز بسمتي",
+      prices: ["30", "40"],
+    },
+    {
+      id: 4,
       name: "طبق بطاطس",
       prices: ["10", "15"],
     },
     {
-      id: 4,
+      id: 5,
       name: "طبق بطاطس بالجبنة",
       prices: ["20", "25"],
     },
-    {
-      id: 5,
-      name: "طبق مخلل مشكل ",
-      prices: ["5", "10"],
-    },
-    {
-      id: 6,
-      name: "1 قطعة فلافل",
-      prices: ["1", ""],
-    },
   ];
-  const drinks = [
+  const makarony = [
     {
       id: 1,
-      name: "مياه معدنية",
-      prices: ["7", ""],
+      name: " معكرونة سادة",
+      prices: ["40", ""],
     },
     {
       id: 2,
-      name: "كولا كانز",
-      prices: ["11", ""],
+      name: "معكرونة جبنة موزريلا",
+      prices: ["50", ""],
     },
     {
       id: 3,
-      name: "كولا لتر",
-      prices: ["20", ""],
+      name: "خضار ( جبنة + فلفل + زيتون ) ",
+      prices: ["60", ""],
     },
     {
       id: 4,
-      name: "كولا عائلي",
-      prices: ["25", ""],
+      name: "نجريسكو",
+      prices: ["70", ""],
+    },
+    {
+      id: 5,
+      name: "شاورما فراخ",
+      prices: ["70", ""],
+    },
+    {
+      id: 6,
+      name: "بانيه",
+      prices: ["70", ""],
+    },
+    {
+      id: 7,
+      name: "سجق",
+      prices: ["60", ""],
+    },
+    {
+      id: 8,
+      name: "هوت دوج",
+      prices: ["60", ""],
+    },
+  ];
+  const kreb = [
+    {
+      id: 1,
+      name: "كريب بطاطس",
+      prices: ["35", ""],
+    },
+    {
+      id: 2,
+      name: "كريب ميكس جبن",
+      prices: ["45", ""],
+    },
+    {
+      id: 3,
+      name: "كريب سجق",
+      prices: ["50", ""],
+    },
+    {
+      id: 4,
+      name: "كريب هوت دوج",
+      prices: ["50", ""],
+    },
+    {
+      id: 5,
+      name: "كريب شاورما",
+      prices: ["65", ""],
+    },
+    {
+      id: 6,
+      name: "كريب كريسبي",
+      prices: ["65", ""],
+    },
+    {
+      id: 7,
+      name: "كريب بانيه",
+      prices: ["65", ""],
+    },
+    {
+      id: 8,
+      name: "",
+      prices: ["", ""],
+    },
+    {
+      id: 9,
+      name: "كريب زنجر",
+      prices: ["65", ""],
+    },
+    {
+      id: 10,
+      name: "كريب تشكن رول",
+      prices: ["65", ""],
+    },
+    {
+      id: 11,
+      name: "كريب شيش طاووق",
+      prices: ["65", ""],
+    },
+    {
+      id: 12,
+      name: "كريب مكسيكي",
+      prices: ["65", ""],
+    },
+    {
+      id: 13,
+      name: "",
+      prices: ["", ""],
+    },
+    {
+      id: 14,
+      name: "كريب فاهيتا",
+      prices: ["70", ""],
+    },
+    {
+      id: 15,
+      name: "كريب كرانشي",
+      prices: ["75", ""],
+    },
+    {
+      id: 16,
+      name: "كريب برجر فراخ",
+      prices: ["70", ""],
+    },
+    {
+      id: 17,
+      name: "كريب برجر لحم",
+      prices: ["80", ""],
+    },
+    {
+      id: 18,
+      name: "كريب ميكس الهيثم",
+      prices: ["80", ""],
+    },
+    {
+      id: 19,
+      name: "كريب ميكس فراخ",
+      prices: ["75", ""],
+    },
+    {
+      id: 20,
+      name: "كريب ميكس لحوم",
+      prices: ["80", ""],
+    },
+    {
+      id: 21,
+      name: "كريب نوتلا",
+      prices: ["50", ""],
+    },
+    {
+      id: 22,
+      name: "كريب نوتلا موز",
+      prices: ["55", ""],
+    },
+    {
+      id: 23,
+      name: "كريب نوتلا بوريو",
+      prices: ["60", ""],
     },
   ];
   const meals = [
@@ -246,22 +352,22 @@ const Tables2 = () => {
     {
       id: 1,
       name: "فرخة كاملة مشوية",
-      prices: ["220", ""],
+      prices: ["230", ""],
     },
     {
       id: 2,
       name: "نص فرخة مشوية ",
-      prices: ["110", ""],
+      prices: ["115", ""],
     },
     {
       id: 3,
       name: "ورك مشوي",
-      prices: ["60", ""],
+      prices: ["65", ""],
     },
     {
       id: 4,
       name: "صدر مشوي ",
-      prices: ["65", ""],
+      prices: ["75", ""],
     },
   ];
   const burger = [
@@ -357,17 +463,17 @@ const Tables2 = () => {
     {
       id: 4,
       name: "فتة شاورما بالجبنة",
-      prices: ["75", ""],
+      prices: ["85", ""],
     },
     {
       id: 5,
       name: "وجبة شاورما عربي",
-      prices: ["85", ""],
+      prices: ["70", ""],
     },
     {
       id: 6,
       name: "وجبة شاورما عربي دبل",
-      prices: ["120", ""],
+      prices: ["130", ""],
     },
   ];
   return (
@@ -445,7 +551,7 @@ const Tables2 = () => {
         <div className="w-full flex flex-col items-center">
           <Image src={im3} alt="" width={400} height={400} />
           <Table className=" m-auto w-11/12">
-            <TableCaption>اطباق</TableCaption>
+            <TableCaption>معكرونة</TableCaption>
             <TableHeader>
               <TableRow>
                 <TableHead className="text-left">السعر</TableHead>
@@ -453,7 +559,7 @@ const Tables2 = () => {
                 <TableHead className="text-right">الاسم</TableHead>
               </TableRow>
             </TableHeader>
-            {plates.map((plate) => {
+            {makarony.map((plate) => {
               return (
                 <TableBody key={plate.id}>
                   <TableRow className="">
@@ -475,8 +581,9 @@ const Tables2 = () => {
           </Table>
         </div>
         <div className=" w-full flex flex-col items-center">
+          <Image src={im9} alt="" width={400} height={400} />
           <Table className=" m-auto w-11/12">
-            <TableCaption>المشروبات</TableCaption>
+            <TableCaption>كريب</TableCaption>
             <TableHeader>
               <TableRow>
                 <TableHead className="text-left">السعر</TableHead>
@@ -484,11 +591,10 @@ const Tables2 = () => {
                 <TableHead className="text-right">الاسم</TableHead>
               </TableRow>
             </TableHeader>
-            {drinks.map((drink) => {
+            {kreb.map((drink) => {
               return (
                 <TableBody key={drink.id}>
                   <TableRow className="">
-                    {/* {sandwich.prices.map((p) => {teturn})} */}
                     {drink.prices?.map((price) => {
                       return (
                         <TableCell key={drink.id} className="text-left">
@@ -504,7 +610,6 @@ const Tables2 = () => {
               );
             })}
           </Table>
-          {/* <Image src={im} alt="" width={300} height={300} /> */}
         </div>
       </div>
       <div className="flex flex-col mt-5 mb-5 w-full md:flex-row">
